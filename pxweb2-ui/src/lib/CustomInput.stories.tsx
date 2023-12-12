42335c308751e4f2eb18d2ec4ac767f4613f1d1f
cklinton <60175636+cklinton@users.noreply.github.com>
2023-12-12 14:40:34 +0100

    adde costomInput

---
+++ pxweb2-ui/src/lib/CustomInput.stories.tsx
@@ -0,0 +1,8 @@
+import CustomInput from './CustomInput'
+
+export default {
+    title: 'Input',
+    component: CustomInput,
+  }
+
+  export const sample = (type:'text', value: "tina") => <CustomInput></CustomInput>;
